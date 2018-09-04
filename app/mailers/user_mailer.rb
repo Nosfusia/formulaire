f587046717871c60e5a1b1c2a45e915b62282742
Sylvain Delva <sylvaindelva@hotmail.fr>
2018-09-04 11:51:03 +0200

    test 1

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-include SendGrid
-default from: 'notifications@example.com'
-
-  def welcome(email)
-    mail(to: email, subject: 'Welcome to My Awesome Site')
-  end
-end
