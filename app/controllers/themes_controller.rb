a57e8751478aadb3c38c70d28af665b286641629
Sylvain Delva <sylvaindelva@hotmail.fr>
2018-09-12 15:14:51 +0200

    test prod du pdf

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -29,6 +29,7 @@ class ThemesController < ApplicationController
     gon.themes = Theme.pluck(:name)
 
     @values = []
+    @theme_points = []
     @themes.each do |theme|
       valeur_theme = 0
       theme.questions.each do |question|
@@ -38,9 +39,38 @@ class ThemesController < ApplicationController
         end
       end
       @values << valeur_theme
+      @theme_points << {name: theme.name, valeur: valeur_theme}
     end
+
+    theme_max = @theme_points.sort_by{|x| x[:value]}.first
+    phrases = [{name: "Gateaux", value:"merde"}]
+    @phrase = phrases.find{|x| x[:name] == theme_max[:name]}[:value]
+
     gon.values = @values
     puts(@values).inspect
+
+    # respond_to do |format|
+    #   format.html
+    #   format.pdf do
+    #     pdf = AnswerPdf.new(@theme)
+    #     send_data pdf.render, filename: 'answer.pdf', type: 'application/pdf', disposition: "inline"
+    #   end
+    # end
+
+
+    respond_to do |format|
+    format.html
+    format.pdf do
+      render  :pdf => 'answer.html.erb',
+              :template => 'themes/answer.html.erb',
+              :layout => "application.html.erb",
+              :page_size => "A4",
+              javascript_delay: 1000, # The relevant line is this one
+              options: {
+              responsive: false
+              }
+      end
+    end
   end
 
   def mail
--- app/javascript/packs/radar.js
+++ app/javascript/packs/radar.js
@@ -10,9 +10,14 @@ var myRadarChart = new Chart(ctx, {
         backgroundColor: 'rgba(00, 255, 00, 0.1)',
         borderColor: '#00FF00',
         borderWidth: 2,
-        data: gon.values
+        data: gon.values,
+        animateScale : false,
+        animation : false,
+        onAnimationComplete : null,
+        javascript_delay: 1000
       }
     ]
   }
 });
 
+console.log("je passe ici")
