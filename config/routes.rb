527a25e19792ac71163fff0938b7a7ff3c728e9f
Sylvain Delva <sylvaindelva@hotmail.fr>
2018-09-12 18:17:24 +0200

    Architecture propre

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 resources :themes do
-  get 'answer'
-  post 'mail'
+  collection do
+    get 'answer'
+    post 'mail'
+  end
 end
 resources :questions
 resources :reponses
   root to: 'themes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
